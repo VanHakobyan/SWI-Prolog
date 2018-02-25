0512fdf7af93bdf33f4ec0f0a3be12d78a3eca75
Vanik Hakobyan <vanhakobyan1996@gmail.com>
2018-02-25 18:36:04 +0400

    Update studies.pl

--- Learning/studies.pl
+++ Learning/studies.pl
@@ -5,7 +5,6 @@ studies(olivia, csc135).   % olivia studies csc135
 studies(jack, csc131).     % jack studies csc131
 studies(arthur, csc134).   % arthur studies csc134
 
-
 teaches(kirke, csc135).   % kirke teaches csc135
 teaches(collins, csc131). % collins teaches csc131
 teaches(collins, csc171). % collins teaches csc171
@@ -25,6 +24,7 @@ teaches(X, C), studies(Y, C).
 
 % charlie studies what? OR
 %What does charlie study?
+
 % ?- professor(kirke, Students).
 
 % Who are the students of professor kirke.  
